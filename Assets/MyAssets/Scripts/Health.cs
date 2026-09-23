c21af0ba00d486b2afe11abb55ad08de30ac1509
JiWoo Kim <wldnkim00@gmail.com>
2026-09-23 16:59:30 +0900

    피드백 받고 다시 풀기 및 테스트 완료

--- Assets/MyAssets/Scripts/Health.cs
+++ Assets/MyAssets/Scripts/Health.cs
@@ -1,45 +1,18 @@
 using System;
 using UnityEngine;
 
-namespace Assets.MyAsset.Scripts
+namespace Assets.MyAsset.Scripts // 이건 내가 코드 스타일이야, 존중해줘
 {
     public sealed class Health : MonoBehaviour
     {
         [SerializeField] private int _maxHealth = 100;
 
         private int _currentHealth;
-        public int CurrentHealth
-        {
-            get
-            {
-                return _currentHealth;
-            }
-            private set
-            {
-                if (!IsDead)
-                {
-                    return;
-                }
-
-                int temp = _currentHealth;
-                _currentHealth = Mathf.Max(_currentHealth + value, 0);
-
-                if (temp != _currentHealth)
-                {
-                    OnChangeHealth?.Invoke(_currentHealth, _maxHealth);
-                }
-
-                if (_currentHealth <= 0)
-                {
-                    IsDead = true;
-                    OnDead?.Invoke();
-                }
-            }
-        }
+        public int CurrentHealth => _currentHealth;
 
         public bool IsDead { get; private set; }
 
-        public event Action<int, int> OnChangeHealth;
+        public event Action<int, int> OnHealthChanged;
         public event Action OnDead;
 
         private void Awake()
@@ -54,7 +27,7 @@ namespace Assets.MyAsset.Scripts
             {
                 return;
             }
-            CurrentHealth = -amount;
+            ApplyDelta(-amount);
         }
 
         public void Heal(int amount)
@@ -64,7 +37,36 @@ namespace Assets.MyAsset.Scripts
                 return;
             }
 
-            CurrentHealth = amount;
+            ApplyDelta(amount);
+        }
+
+        private void ApplyDelta(int delta)
+        {
+            if (IsDead)
+            {
+                return;
+            }
+
+            int temp = _currentHealth;
+            _currentHealth = Mathf.Clamp(_currentHealth + delta, 0, _maxHealth);
+
+            if (temp != _currentHealth)
+            {
+                OnHealthChanged?.Invoke(_currentHealth, _maxHealth);
+                Debug.Log("체력 변화 이벤트 실행");
+            }
+
+            if (_currentHealth <= 0)
+            {
+                IsDead = true;
+                OnDead?.Invoke();
+                Debug.Log("사망 이벤트 실행");
+            }
+        }
+
+        public void HpLog()
+        {
+            Debug.Log(CurrentHealth.ToString());
         }
     }
 }
